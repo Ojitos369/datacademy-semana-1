97c4b9815589b9c147dbc323a355c65df87c1f66
ojitos369 <ojitos369@gmail.com>
2021-05-14 23:37:07 -0500

    Problema del triangulo

---
+++ triangulo/main.py
@@ -0,0 +1,23 @@
+def calcular_area(base, altura):
+    return base * altura / 2
+
+def comparacion(lado_a, lado_b, lado_c):
+    if lado_a == lado_b and lado_b == lado_c:
+        print('Triangulo Equilatero')
+    elif lado_a != lado_b and lado_b != lado_c and lado_a != lado_c:
+        print('Triangulo Escaleno')
+    else:
+        print('Triangulo Isoseles')
+
+def main():
+    base = float(input('Ingresa la base: '))
+    altura = float(input('Ingresa la altura: '))
+    lado_a = float(input('Ingresa la lado A: '))
+    lado_b = float(input('Ingresa la lado B: '))
+    lado_c = float(input('Ingresa la lado C: '))
+    area = calcular_area(base, altura)
+    print(f'El area es: {area}')
+    comparacion(lado_a, lado_b, lado_c)
+
+if __name__ == '__main__':
+    main()
